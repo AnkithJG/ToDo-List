9a0e2d7ceed980915ab7592d65b5ab610af4a1bf
Ankith Ganji <ganjiankith@gmail.com>
2022-10-15 18:35:26 -0500

    Initial Commit

---
+++ ToDo-List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo-List
+//
+//  Created by AG on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
